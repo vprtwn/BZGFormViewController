097954e2e5a8d4236463ca66497159ddcf304e76
Dasmer Singh <dasmer.singh@venmo.com>
2014-04-08 09:59:49 -0400

    Complete tests

---
+++ BZGFormViewControllerTests/BZGKeyboardControlSpecs.m
@@ -0,0 +1,34 @@
+//
+//  BZGKeyboardControlSpecs.m
+//  SignupForm
+//
+//  Created by Dasmer Singh on 4/8/14.
+//  Copyright (c) 2014 benzguo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BZGKeyboardControlSpecs : XCTestCase
+
+@end
+
+@implementation BZGKeyboardControlSpecs
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
